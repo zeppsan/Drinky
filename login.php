d989f6ed345f7c715fb15d724c81f3845253b3b6
eric_ <eric_@DESKTOP-HIE4FH2>
2020-10-13 21:06:48 +0200

    added login styling

--- login.php
+++ login.php
@@ -6,35 +6,38 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
 </head>
-<body>
+<body id="loginBody">
     <?php include_once 'header.php'; ?>
-    <div class="container">
-        <div class="row mt-5">
-                <div class="col-12 col-md-8 mx-auto">
-                    <form action="php/account/login.inc.php" method="POST">
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    <label for="exampleInputUsername">Username</label>
-                                    <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="usernameHelp" placeholder="Enter username" name="username" required>
-                                </div>
+
+    <div class="container-fluid" id="login-container">
+        <div class="row">
+            <div class="col-12 col-md-6 text-center">
+                <h2>Drinky Login</h1>
+                <h3>Welcome back you alcoholic!</h3>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4 registration-form py-5 loginform-form">
+                <form action="php/account/login.inc.php" method="POST">
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="exampleInputUsername">Username</label>
+                                <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="usernameHelp" placeholder="Enter username" name="username" required>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col">
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password" required>
-                                </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password" required>
                             </div>
-                        </div>    
-                        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-                    </form>
-                </div>
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-primary" name="submit">Login</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-
     <script src="js/jquery-3.5.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
