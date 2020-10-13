19a6d32c213a6fe2578e43d98e6c42ab9d771f94
Frida Westerlund <fwd19002@student.mdh.se>
2020-10-13 14:44:34 +0200

    La över <head> till headern, behövs bara include

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Drinky</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-
-</head>
 <body>
     <?php include_once 'header.php'; ?>
     <div class="container">
