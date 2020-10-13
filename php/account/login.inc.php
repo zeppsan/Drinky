6c001073392494c42c6853a8aa2c4ebed77835a9
eric_ <eric_@DESKTOP-HIE4FH2>
2020-10-13 16:59:08 +0200

    fixed database connection string

---
+++ php/account/login.inc.php
@@ -0,0 +1,47 @@
+<?php
+
+
+    /* 
+        Author: 
+            Eric Qvarnström
+
+        Description:
+            Script to login a user. Checks credentials agains database
+        
+        Variables in:
+            username        - Username
+            password        - Password
+
+    */ 
+    
+    include_once '../includes/db.inc.php';
+
+    // Checks so that username and password is set
+    if(!isset($_POST['username']) || !isset($_POST['password'])){
+        header('Location: ../../login.php?error=notfilled');
+    }
+
+    // Select id, username and password from database where username is = to the input 
+    $stmt = $conn->prepare("SELECT id, username, pwd FROM users WHERE username=?");
+    $stmt->bind_param("s", $_POST['username']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    // check if the query returned any rows
+    if($result->num_rows != 1){
+        header('Location: ../../login.php?error=wrongcredentials');
+    } else {
+        // check if the password is correct
+        $row = $result->fetch_assoc();
+        if(password_verify($_POST['password'], $row['pwd'])){
+            // Password is correct. Log the user in.
+            session_start();
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['id'] = $row['id'];
+            header("location: ../../index.php?loggedin");
+        } else {
+            // Password is worng, hit him hard. 
+            header("Location: ../../login.php?error=wrongcredentials");
+        }
+    }
+?>
