36bead3c6bd8aa730801c68395527370efa76afb
Frida Westerlund <fwd19002@student.mdh.se>
2020-10-16 16:08:24 +0200

    .

--- header.php
+++ header.php
@@ -24,21 +24,25 @@
     <div class="appName"><a href="index.php"><h1>DRINK</h1><img id="cocktail" src="media/coctail.png" width="45px" height="40px"></a></div> 
     
     <nav class="nav justify-content-center"> 
-        <!-- <a class="nav-link active" href="index.php">Start</a> -->
-        <a class="nav-link active" href="#">Search Recipe</a>
-        <a class="nav-link" href="add_recipe.php">Make Recipe</a>
-        <a class="nav-link" href="#">Random Recipe</a> 
-        <?php 
-          if(isset($_SESSION['username'])){ 
-            echo '<a class="nav-link" href="profile.php">My Profile</a>';
-            echo '<a class="nav-link" href="php/account/logout.inc.php">Log Out</a>';
-          }
-        ?>
-        <?php 
-          if(!isset($_SESSION['username'])){ 
-            echo '<a class="nav-link" href="login.php">Login</a>';
-          }
-        ?>
+      <!-- <a class="nav-link active" href="index.php">Start</a> -->
+      <a class="nav-link active" href="#">Search Recipe</a>
+      <a class="nav-link" href="add_recipe.php">Make Recipe</a>
+      <a class="nav-link" href="#">Random Recipe</a> 
+      <?php 
+        if(isset($_SESSION['username'])){ 
+          echo '<ul class="nav nav-tabs"><li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" ><img src="media/myprofile.png" width="25px" height="25px"></a>
+          <div class="dropdown-menu"> 
+          <a class="dropdown-item" href="profile.php">My Profile</a>
+          <a class="dropdown-item" href="php/account/logout.inc.php">Log Out</a>
+          </div></li></ul>';
+        }
+      ?>
+      <?php 
+        if(!isset($_SESSION['username'])){ 
+          echo '<a class="nav-link" href="login.php">Login</a>';
+        }
+      ?>
     </nav> 
     <!-- sökrutan -->
     <div class="input-group mb-3"> 
