3537ff9453f095be58c57b0d98f99416a07df09e
YouKnow-Casper <casper.karrstrom@hotmail.com>
2020-10-16 15:56:29 +0200

    huh

--- showRecipe.php
+++ showRecipe.php
@@ -1,18 +1,18 @@
 <?php
 
-include("./header.php");
 include("./php/includes/db.inc.php");
 
-//session_start();
-$drinkName = "Crispy Pear";
+session_start();
+if(!isset($_SESSION['username']))
+    header("Location: login.php");
+
+$drinkName = "Secret Mixture";
 
 // Fetch information about the specified recipe
 $stmt = $conn->prepare("SELECT * FROM recipe WHERE name = ?");
 $stmt->bind_param("s", $drinkName); //$_POST['drinkName']
 $stmt->execute();
 $drink = $stmt->get_result()->fetch_assoc();
-
-//print_r($drink); die();
 
 // Fetch information about the user that created the recipe
 //$stmt = $conn->prepare("SELECT username, profile_picture FROM users WHERE ...");
@@ -22,14 +22,14 @@ $stmt->bind_param("s", $drink['recipe_ID']);
 $stmt->execute();
 $user = $stmt->get_result()->fetch_assoc();
 
-//print_r($user); die();
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-    
+    <title>Recepie</title>
+    <?php include_once 'header.php'; ?>
 </head>
 
 <body>
@@ -42,8 +42,9 @@ $user = $stmt->get_result()->fetch_assoc();
     <!--    <img src="<?php //echo $drink['imgurl'] ?>" >        Image of drink  -->
     <h2><?php echo $drink['name']?></h2>
     <!--    Drink Rating    -->
-    <p><?php echo $drink['votes'] ?></p>
-    <div></div>         <!--    Yellow color for stars  -->
+    <h2>Rating: <?php echo $drink['rating_total']/$drink['votes'] ?></h2>
+    <p>Number of votes: <?php echo $drink['votes'] ?></p>      
+    <!--    Yellow color for stars  -->
     <img src="" >       <!--    Cut out stars image   -->
     <p><?php echo $drink['description'] ?></p>
 </div>
@@ -62,11 +63,11 @@ $user = $stmt->get_result()->fetch_assoc();
 
 <!--    Drink Creator... Name, rating, link -->
 <div>
-    <img src="<?php echo $user['profile_picture'] ?>" >   <!--    User picture    -->
+    <div class="rounded-circle" id="profile_picture">
+        <img src="<?php echo $user['profile_picture'] ?>" width="100%">   <!--    User picture    -->
+    </div>
     <a href="http://localhost/Drinky/profile.php?user=<?php echo $user['username'] ?>"><?php echo $user['username'] ?></a>
-    <p> <?php echo $user['fname'] ?></p>
-    <p> <?php echo $user['lname'] ?></p>
-    <p> <?php echo $user['age'] ?></p>
+    <p> <?php echo $user['fname'], ' ', $user['lname'], ' ', $user['age'] ?></p>
     <!--    User Rating    -->
     <div></div>     <!--    Yellow color for stars  -->
     <img src="" >   <!--    Cut out for stars   -->
