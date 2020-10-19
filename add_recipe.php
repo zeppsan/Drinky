5e3b5e14aab433386fa27a92d0f3edb6259c8337
Maggan <maxjon01@gmail.com>
2020-10-19 14:33:43 +0200

    Går att skriva utan att text försvinner på recipe

--- add_recipe.php
+++ add_recipe.php
@@ -44,9 +44,9 @@
                         <textarea id="instructions" name="instructions" rows="10" cols="30"></textarea>
                     </p>
 
-                    <div id="Make_Recipe_Ingredients">
+                    <div id="Recipe_Ingredients">
 
-                        <div class="input-group mb-3">
+                        <div class="input-group mb-3" id="ingredient-group-0">
                             <div class="input-group-prepend">
                                 <span class="input-group-text">Spirit</span>
                             </div>
@@ -66,7 +66,7 @@
 
                     <div class="row">
                         <div class="col text-center">
-                            <a href="#" onclick="addIngerdient()"><img src="media/plus_icon.png" width="32px"></a>
+                            <a href="#" onclick="addField()"><img src="media/plus_icon.png" width="32px"></a>
                         </div>
                     </div>
 
@@ -79,37 +79,3 @@
     <script src="js/add_recipe.js"></script>
 </body>
 </html>
-
-
-
-
-
-<!--
-<!DOCTYPE html>
-<html>
-
-<head>
-
-</head>
-
-<body id="addRecipeBody">
--->    <!-- Div for the AJAX serach results -->
-<!--    <div id="Ingredient_Lookup_Div"></div>
--->
-    <!-- Temp form for adding data to the database. 
-        In future changes the name property on every input/textbox needs to stay the same for the backend to work -->
-<!--    <form id="add_recipe_form" name="recipe_form" action="./php/recipe/add_recipe_to_db.php" method="POST">
-        Name of drink <input type="text" name="drinkName" id="name" required><br>
-        Short description <textarea id="description" name="description" rows="2" cols="30" required></textarea><br>
-        Instructions <textarea id="instructions" name="instructions" rows="4" cols="50" required></textarea><br>
-        Centiliter <input type="number" name="ingredient[0][amount]" id="amount_0" required><br>
-        Sprit <input list="list_ingredient_0" type="text" name="ingredient[0][ingredientName]" id="ingredient_0"
-            onkeyup="Ingredient_Lookup(this.value, this.id)" autocomplete="off" required><br>
-    </form>
-    <button type="submit" form="add_recipe_form" value="Submit">Submit</button>   
-    <input type="button" value="Add Ingredient" onclick="addIngredient()">
-
-    <script src="./js/add_recipe.js"></script>
-    </body>
-
-</html>-->
