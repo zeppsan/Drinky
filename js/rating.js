6a3dde0c1c7ead57bab950322e128f268ce26f13
YouKnow-Casper <casper.karrstrom@hotmail.com>
2020-10-19 16:30:37 +0200

    omg saker

---
+++ js/rating.js
@@ -0,0 +1,35 @@
+/* 
+Author: 
+    Casper Kärrström
+
+Description:
+    Javascript for the rating system on recipes
+
+*/
+
+document.addEventListener("click", (e) => {
+
+    switch (e.target.id) {
+        case "s1":
+            rate(1);
+            break;
+        case "s2":
+            break;
+        case "s3":
+            break;
+        case "s4":
+            break;
+        case "s5":
+            break;
+        default:
+            break;
+    }
+
+});
+
+
+function rate(rating){
+
+    
+
+}
